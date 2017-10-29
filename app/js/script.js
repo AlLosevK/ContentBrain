d69d0dd5925a3d56c3974e94e2c40d9d19ded870
AlLosevK <al.losev.k@gmail.com>
2017-10-29 15:32:42 +0300

    all without coretopic3

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,21 @@
-(document).ready(function () {
+$(document).ready(function () {
     $('.approvecontent2__button-item').on('click', function () {
-        $('.approvecontent2__button-item').removeClass('approvecontent2__button');
+        $('.approvecontent2__button-item').removeClass('approvecontent2__button-active');
         $(this).addClass('approvecontent2__button-active');
 
-        if ($('.approvecontent2__button-item').hasClass("approvecontent2__button-active")) {
-            $('.resources').fadeIn(10);
+        if ($('.js-show__button-things').hasClass("approvecontent2__button-active")) {
+            $('.resources-item').fadeOut(0);
+            $('.js-show-block-thing').fadeIn(10);
+        }
+
+        if ($('.js-show__button-how').hasClass("approvecontent2__button-active")) {
+            $('.resources-item').fadeOut(0);
+            $('.js-show-block-how').fadeIn();
+        }
+
+        if ($('.js-show__button-introdution').hasClass("approvecontent2__button-active")) {
+            $('.resources-item').fadeOut(0);
+            $('.js-show-block-introdution').fadeIn();
         }
     })
 })
