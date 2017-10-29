ee82127bb46de9597ec0b7c581a73f92392eec5b
AlLosevK <al.losev.k@gmail.com>
2017-10-29 13:55:50 +0300

    80%

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,10 @@
-var W3CDOM = (document.createElement && document.getElementsByTagName);
+(document).ready(function () {
+    $('.approvecontent2__button-item').on('click', function () {
+        $('.approvecontent2__button-item').removeClass('approvecontent2__button');
+        $(this).addClass('approvecontent2__button-active');
 
-function initFileUploads() {
-	if (!W3CDOM) return;
-	var fakeFileUpload = document.createElement('div');
-	fakeFileUpload.className = 'fakefile';
-	fakeFileUpload.appendChild(document.createElement('input'));
-	var image = document.createElement('img');
-	image.src='pix/button_select.gif';
-	fakeFileUpload.appendChild(image);
-	var x = document.getElementsByTagName('input');
-	for (var i=0;i<x.length;i++) {
-		if (x[i].type != 'file') continue;
-		if (x[i].parentNode.className != 'fileinputs') continue;
-		x[i].className = 'file hidden';
-		var clone = fakeFileUpload.cloneNode(true);
-		x[i].parentNode.appendChild(clone);
-		x[i].relatedElement = clone.getElementsByTagName('input')[0];
-		x[i].onchange = x[i].onmouseout = function () {
-			this.relatedElement.value = this.value;
-		}
-	}
-}
+        if ($('.approvecontent2__button-item').hasClass("approvecontent2__button-active")) {
+            $('.resources').fadeIn(10);
+        }
+    })
+})
